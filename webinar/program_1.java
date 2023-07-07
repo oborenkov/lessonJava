fba669910bdadf7e98a82cc4b0e1d47e01e15f91
oborenkov <[oborenkov@yahoo.com]>
2023-07-07 08:37:07 +0200

    nov

---
+++ webinar/program_1.java
@@ -0,0 +1,19 @@
+/**
+ * program_1
+ */
+public class program_1 {
+
+    public static void main(String[] args) {
+
+        int salery = 123456;
+        short age = 10;
+        System.out.println(salery);
+        System.out.println(age);
+        float e = 2.7f;
+        double pi = 3.1415;
+        System.out.println(e);
+        System.out.println(pi);
+        System.out.println();
+
+    }
+}
